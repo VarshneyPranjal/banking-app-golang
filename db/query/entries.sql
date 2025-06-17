ad3a7fb7f304610813f3386ce6ebf58b66a7e8f9
Pranjal_Varshney <pranjal_varshney@apple.com>
2025-06-18 01:35:41 +0530

    similarly, generate all remaining CRUD operations for entries table

--- db/query/entries.sql
+++ db/query/entries.sql
@@ -5,3 +5,23 @@ INSERT INTO entries (
 ) VALUES (
   $1, $2
 ) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateEntry :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteEntry :exec
+DELETE FROM accounts
+WHERE id = $1;
